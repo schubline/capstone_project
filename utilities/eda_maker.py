c22855c7ccd217e811012fa864286926c55232ea
Shlafor <laforest.hervey@gmail.com>
2018-07-11 20:40:17 -0400

    script to produce eda results at scale

---
+++ utilities/eda_maker.py
@@ -0,0 +1,13 @@
+"""
+The goal of this script to to automate the EDA process so that I can apply it
+to different Yelp Datasets I create (subsectionning by city, business type etc)
+"""
+def clean_data(data):
+    pass
+
+def make_features(data):
+    pass
+
+
+def make_plots(data):
+    pass
